d0a2d561298ff8b32281a01f795fd9fc0272b11d
Dalibor Bilický <d.bilicky@gmail.com>
2025-01-05 17:05:35 +0100

    I realized i need FIFO even when LIFO is easy implemenation

--- server/src/sync_buffer/buffer.c
+++ server/src/sync_buffer/buffer.c
@@ -3,10 +3,12 @@
 #include <string.h>
 
 void buffer_init(Buffer* self, size_t capacity, size_t data_size) {
-    self->data = calloc(capacity, data_size);
+    self->data = calloc(capacity, sizeof(data_size));
     self->data_size = data_size;
     self->capacity = capacity;
     self->size = 0;
+    self->in = 0;
+    self->out = 0;
 }
 
 void buffer_destroy(Buffer* self) {
@@ -15,13 +17,17 @@ void buffer_destroy(Buffer* self) {
 }
 
 void buffer_push(Buffer* self, const void* input) {
-    memcpy((char*)self->data + self->size * self->data_size, input,
+    memcpy((char*)self->data + self->in * self->data_size, input,
            self->data_size);
     self->size++;
+    self->in++;
+    self->in %= self->capacity;
 }
 
 void buffer_pop(Buffer* self, void* output) {
-    self->size--;
-    memcpy(output, (char*)self->data + self->size * self->data_size,
+    memcpy(output, (char*)self->data + self->out * self->data_size,
            self->data_size);
+    self->size--;
+    self->out++;
+    self->out %= self->capacity;
 }
